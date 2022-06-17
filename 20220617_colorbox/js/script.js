077130e11dc7943989da282e72ef01f46a85c484
yeonji0 <kyj0279@gmail.com>
2022-06-17 16:42:14 +0900

    c

---
+++ 20220617_colorbox/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".modal").colorbox({
+        rel:'modal'
+    })
+    $(".youtube").colorbox({
+        iframe:true,
+        innerWidth:700,
+        innerHeight:394
+    })
+});

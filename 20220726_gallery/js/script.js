e6f64d991cefda8d7ea2511aabeb2d24ae052af2
yeonji0 <kyj0279@gmail.com>
2022-07-26 15:34:17 +0900

    d

--- 20220726_gallery/js/script.js
+++ 20220726_gallery/js/script.js
@@ -1,27 +1,27 @@
-
 $(".itemWrap>li").click(function(){
     console.log($(this).index());
-
+    
     let i = $(this).index();
     $("#image>p").hide();
     $("#image>p").eq(i).show();
 })
 
-let current=0;
+let current = 0;
 $(".right").click(function(){
     current++;
-    if(current == 3){current = 2}
+    if(current == 3) { current = 2 }
     console.log(current);
-    let value = current * -760;
-    // $(".itemWrap").css("left",current*-760)
-    $(".itemWrap").animate({"left":value},300)
+    view(current)
 })
 
 $(".left").click(function(){
     current--;
-    let value = current * -760;
-    // $(".itemWrap").css("left",current*-760)
-    $(".itemWrap").animate({"left":value},300)
+    if(current < 0){current = 0}
+    view(current)
 })
 
-
+function view(current){
+    let value = current * -760;
+    // $(".itemWrap").css("left",value)
+    $(".itemWrap").stop().animate({"left":value},300)
+}

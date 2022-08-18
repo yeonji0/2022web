db2b21836eb5dfeb838bdd27f790bbd07f3dcc57
yeonji0 <kyj0279@gmail.com>
2022-08-18 15:36:34 +0900

    s

--- 20220818_scrollmenu/js/script.js
+++ 20220818_scrollmenu/js/script.js
@@ -15,5 +15,36 @@ $(".menuWrap > a").click(function(e){
     let i = $(this).index();
     let st = $(".scene").eq(i).offset().top;
     $("html,body").stop().animate({scrollTop:st})
+});
+
+$(window).on("scroll",function(){
+    // let scrollY = $(window).scrollTop();
+    let scrollY = window.pageYOffset;
+    console.log(scrollY)
+
+    // if(oneTop <= scrollY){
+    //     $(".menuWrap a").removeClass("active").eq(0).addClass("active")
+    // }
+    // if(twoTop <= scrollY){
+    //     $(".menuWrap a").removeClass("active").eq(1).addClass("active")
+    // }
+    // if(threeTop <= scrollY){
+    //     $(".menuWrap a").removeClass("active").eq(2).addClass("active")
+    // }
+
+    // for (let i=0;i<$(".scene").length;i++){
+    //     if( $(".scene").eq(i).offset().top <= scrollY){
+    //     $(".menuWrap a").removeClass("active").eq(i).addClass("active");
+    //     }
+    // }
+
+    $(".scene").each(function(index){
+        if( $(this).offset().top <= scrollY){
+            $(".menuWrap a").removeClass("active").eq(index).addClass("active");
+         }
+    })
+
+
+
 })
 

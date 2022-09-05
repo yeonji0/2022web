f060f29a2c9e7596fecc693166747540ddead163
yeonji0 <kyj0279@gmail.com>
2022-09-05 18:05:25 +0900

    d

---
+++ 20220905_fullpage/js/script.js
@@ -0,0 +1,8 @@
+$("#fullpage").fullpage({
+    menu:".navTop",
+    anchor:["m1st","m2st","m3st",".m4st"],
+    navigation:true,
+    navigationPosition:"fp-right",
+    navigationTooltips:["first","second","third","fore"],
+    responsiveWidth:900
+})

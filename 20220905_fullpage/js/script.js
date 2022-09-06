24e351a83954aaaef5596b9e31b45f46f1072af2
yeonji0 <kyj0279@gmail.com>
2022-09-06 15:44:20 +0900

    d

--- 20220905_fullpage/js/script.js
+++ 20220905_fullpage/js/script.js
@@ -4,5 +4,31 @@ $("#fullpage").fullpage({
     navigation:true,
     navigationPosition:"fp-right",
     navigationTooltips:["first","second","third","fore"],
-    responsiveWidth:900
+    responsiveWidth:900,
+    afterLoad:function(anchorslink,index){
+        console.log(anchorslink,index);
+        if(index==1){
+            $(".page1 .txtWrap").addClass("move")
+        }else{
+            $(".page1 .txtWrap").removeClass("move")
+        }
+
+        if(index==2){
+            $(".page2 .txtWrap").addClass("move")
+        }else{
+            $(".page2 .txtWrap").removeClass("move")
+        }
+
+        if(index==4){
+            $(".page4 .txtWrap").addClass("move")
+        }else{
+            $(".page4 .txtWrap").removeClass("move")
+        }
+
+        if(index==3){
+            $(".page3 .wrap").addClass("move")
+        }else{
+            $(".page3 .wrap").removeClass("move")
+        }
+    }
 })
